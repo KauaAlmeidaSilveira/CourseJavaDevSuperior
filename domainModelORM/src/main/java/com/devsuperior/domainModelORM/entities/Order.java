10046f5fcd418684e22a66ae1d8c79cfd0a7df1d
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-12-20 18:32:46 -0300

    Order, Enum and ManyToOne

---
+++ domainModelORM/src/main/java/com/devsuperior/domainModelORM/entities/Order.java
@@ -0,0 +1,79 @@
+package com.devsuperior.domainModelORM.entities;
+
+import com.devsuperior.domainModelORM.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column( columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return client;
+    }
+
+    public void setUser(User user) {
+        this.client = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ domainModelORM/src/main/java/com/devsuperior/domainModelORM/entities/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.devsuperior.domainModelORM.entities.enums;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
