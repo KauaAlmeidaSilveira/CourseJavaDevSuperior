4c31b66f8fbf45489b7f5ae9317fb49cc6b81b7e
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-01-28 14:19:41 -0300

    Exercise 2621 - JPQL

--- JpaConsultasSQL_JPQL/uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ JpaConsultasSQL_JPQL/uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -24,8 +24,15 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<ProductProjection> products = repository.productsSQL("P");
+		List<ProductProjection> products = repository.productsSQL(1, 500,  "E");
 		List<ProductDTO> result1 = products.stream().map(ProductDTO::new).collect(Collectors.toList());
+		System.out.println("SQL");
 		result1.forEach(System.out::println);
+
+		System.out.println("\n\n");
+
+		List<ProductDTO> result2 = repository.productsJPQL(1, 500,  "E");
+		System.out.println("JPQL");
+		result2.forEach(System.out::println);
 	}
 }
--- JpaConsultasSQL_JPQL/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ JpaConsultasSQL_JPQL/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.dto.ProductDTO;
 import com.devsuperior.uri2621.projections.ProductProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,10 +11,17 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT products.name from products " +
+    @Query(nativeQuery = true, value = "SELECT products.name " +
+            "FROM products " +
             "INNER JOIN providers ON products.id_providers = providers.id " +
             "WHERE providers.name LIKE :startsWith% " +
-            "AND products.amount >= 10 AND products.amount <= 20")
-    List<ProductProjection> productsSQL(String startsWith);
+            "AND products.amount BETWEEN :min AND :max")
+    List<ProductProjection> productsSQL(Integer min, Integer max, String startsWith);
 
+
+    @Query("SELECT new com.devsuperior.uri2621.dto.ProductDTO(obj.name) " +
+            "FROM Product obj " +
+            "WHERE obj.provider.name LIKE :startsWith% " +
+            "AND obj.amount BETWEEN :min AND :max")
+    List<ProductDTO> productsJPQL(Integer min, Integer max, String startsWith);
 }
