263e038f462a5085354b5e9586d68be600588ed9
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-03-07 13:07:24 -0300

    BCrypt Password

--- spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,15 +3,28 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()); // Desabilitando Ataques de CSRF, pois não estamos utilizando sessões.
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Permitindo todas as requisições.
+
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        // Desabilitando a proteção contra ataques de Clickjacking (X-Frame-Options).
+        // O que é Clickjacking? É um ataque que consiste em enganar o usuário para que ele clique em um link
+        // que não é o que ele vê, mas sim um botão ou link de um site malicioso.
+
         return http.build(); // Construindo a configuração de segurança.
     }
 
--- spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$uc17x2/vLikJKIT.P0j7u.Wj3dhqGkx/JOmPmF8kJdj9Ypzhy65MS');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$uc17x2/vLikJKIT.P0j7u.Wj3dhqGkx/JOmPmF8kJdj9Ypzhy65MS');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
